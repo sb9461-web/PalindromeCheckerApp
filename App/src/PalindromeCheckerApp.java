92a1c544a5ae67b7453a9b9eea6114347c849d67
sb9461-web <sb9461@srmist.edu.in>
2026-02-26 14:34:39 +0530

    UC1: Application Entry & Welcome Message

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+    static void main() {
+        System.out.println("welcome to the palindrome Checker Managment system");
+        System.out.println("Version ; 1.0");
+        System.out.println("System initalized successfully");
+    }
 }
